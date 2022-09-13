179695e9c96c36577dd515f5c8bdf5a5b4fb0613
Jimmy <jimmy.karlsson@microsoft.com>
2022-09-13 14:52:38 +0200

    added a script to update azure arc agent on servers with the help of scheduled task

---
+++ ARC/AgentManagement/README.md
@@ -0,0 +1,6 @@
+# ARC
+This script creates a scheduled task on a random time between 6 p.m to eleven p.m on the machine that runs a script that downloads Azure Arc agent and verifies if a newer or older version, if newer will install that.
+## Deployment ##
+Execute the script with administrator priviligies on the machine to install the ask and script. It will create a folder C:\arc where the run script will be located.
+
+## Troubleshooting ##
---
+++ ARC/AgentManagement/Update/Create-ArcUpdateTask.ps1
@@ -0,0 +1,183 @@
+<#
+    Creates a scheduled task on the servers that will check for updates each day and update it.
+#>
+
+$script = @({
+[CmdletBinding()]
+param (
+    [string]$OutFile,
+    [string]$AltDownload,
+    [string]$Proxy    
+)
+
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+$refVersion = [version] '4.6'
+
+$global:errorcode="AZCM0150"
+$msifile = 'AzureConnectedMachineAgent.msi'
+
+function Which-MSIVersion {
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = 'Specifies path to MSI file.')][ValidateScript({
+        if ($_.EndsWith('.msi')) {
+            $true
+        } else {
+            throw ("{0} must be an '*.msi' file." -f $_)
+        }
+    })]
+    [String[]] $msifile
+    )
+
+    $invokemethod = 'InvokeMethod'
+    try {
+
+        #calling com object
+        $FullPath = (Resolve-Path -Path $msifile).Path
+        $windowsInstaller = New-Object -ComObject WindowsInstaller.Installer
+
+        ## opening database from file
+        $database = $windowsInstaller.GetType().InvokeMember(
+            'OpenDatabase', $invokemethod, $Null, 
+            $windowsInstaller, @($FullPath, 0)
+        )
+
+        ## select productversion from database
+        $q = "SELECT Value FROM Property WHERE Property = 'ProductVersion'"
+        $View = $database.GetType().InvokeMember(
+            'OpenView', $invokemethod, $Null, $database, ($q)
+        )
+
+        ##execute
+        $View.GetType().InvokeMember('Execute', $invokemethod, $Null, $View, $Null)
+
+        ## fetch
+        $record = $View.GetType().InvokeMember(
+            'Fetch', $invokemethod, $Null, $View, $Null
+        )
+
+        ## write to variable
+        $productVersion = $record.GetType().InvokeMember(
+            'StringData', 'GetProperty', $Null, $record, 1
+        )
+
+        $View.GetType().InvokeMember('Close', $invokemethod, $Null, $View, $Null)
+
+
+        ## return productversion
+        return $productVersion
+
+    }
+    catch {
+        throw 'Failed to get MSI file version the error was: {0}.' -f $_
+    }
+}
+
+###
+try {
+    # Download the package
+    Write-Verbose -Message "Downloading agent package" -Verbose
+    $TlsVersions = [enum]::GetNames('System.Net.SecurityProtocolType') | Where-Object { $_ -ge 'Tls12' }
+    $TlsVersions.ForEach({
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor $_
+    })
+    try {
+        if ($AltDownload) {
+            if ($Proxy) {
+                Invoke-WebRequest -UseBasicParsing -Proxy $Proxy -Uri $AltDownload -OutFile AzureConnectedMachineAgent.msi
+            } else {
+                Invoke-WebRequest -UseBasicParsing -Uri $AltDownload -OutFile AzureConnectedMachineAgent.msi
+            }
+	} else {
+            if ($Proxy) {
+                Invoke-WebRequest -UseBasicParsing -Proxy $Proxy -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi	
+            } else {
+                Invoke-WebRequest -UseBasicParsing -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi	
+            }
+        }
+    }
+    catch {
+        $errorcode="AZCM0148"
+        throw "Invoke-WebRequest failed: $_"
+    }
+    #CHECK VERSION
+    $installedversion =(azcmagent.exe version).Replace('azcmagent version  ','')
+    [string]$newversion = Which-MSIVersion -msifile AzureConnectedMachineAgent.msi
+
+    If([System.Version]$newversion -gt [System.Version]$installedversion)
+    {# Install the package
+        echo "Installing"
+        Write-Verbose -Message "Installing agent package" -Verbose
+        $exitCode = (Start-Process -FilePath msiexec.exe -ArgumentList @("/i", "AzureConnectedMachineAgent.msi" , "/l*v", "installationlog.txt", "/qn") -Wait -Passthru).ExitCode
+        if ($exitCode -ne 0) {
+            $message = (net helpmsg $exitCode)        
+            $errorcode="AZCM0149"
+            throw "Upgrade failed: $message See installationlog.txt for additional details."
+    }
+}
+    else {
+        Write-Verbose -Message "current version is up to date" -Verbose
+        #Remove-Item .\AzureConnectedMachineAgent.msi -Force
+    }
+
+
+    # Check if we need to set proxy environment variable
+    if ($Proxy) {
+        Write-Verbose -Message "Setting proxy configuration: $Proxy" -Verbose
+        & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent" config set proxy.url ${Proxy}
+    }
+    
+} catch {
+    if ($OutFile) {
+        [ordered]@{
+            status  = "failed"
+            error = [ordered]@{
+                code = $errorcode
+                message = $_.Exception.Message
+            }
+        } | ConvertTo-Json | Out-File $OutFile
+    }
+    Write-Error $_ -ErrorAction Continue
+    exit 1
+}
+
+# Installation was successful if we got this far
+if ($OutFile) {
+    [ordered]@{
+        status  = "success"
+        message = "Installation of azcmagent completed successfully"
+    } | ConvertTo-Json | Out-File $OutFile
+}
+
+Write-Host "Installation of azcmagent completed successfully"
+
+exit 0
+})
+
+$filepath = 'C:\arc\arcupdate.ps1'
+$taskname = 'UpdateArcAgent'
+
+#Create file
+If((Test-Path $filepath))
+{
+    echo "$filepath already exists"
+}
+else {
+    New-Item -Path 'C:\arc\arcupdate.ps1' -Force
+}
+
+#Write content to file, for each run it will just replace current content
+Out-File -FilePath 'C:\arc\arcupdate.ps1' -InputObject $script -Force -Encoding utf8
+#$script >> 'C:\arc\arcupdate.ps1'
+
+If((Get-ScheduledTask -TaskName $taskname -ErrorAction SilentlyContinue))
+{
+    echo "task already exists"
+}
+else {
+    $random = Get-Random -Minimum 6 -Maximum 11
+    $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -File "C:\arc\arcupdate.ps1" -Force -NoTypeInformation}"'
+    $trigger =  New-ScheduledTaskTrigger -Daily -At $random"pm"
+
+    Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskname -Description "Check for updates for Arc agent" -RunLevel Highest -Force
+}
